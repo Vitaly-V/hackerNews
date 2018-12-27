7e8c8a1478359138b43fbc7635cec66c845a46b2
Vitaly-V <gtopbox@gmail.com>
2018-12-27 16:04:27 +0200

    Fake future data

---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class NewsList extends StatelessWidget {
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: buildList(),
+    );
+  }
+  
+  Widget buildList() {
+    return ListView.builder(
+        itemCount: 1000,
+        itemBuilder: (context, int index) {
+          return FutureBuilder(
+            future: getFuture(),
+            builder: (context, snapshot) {
+              return Container(
+                height: 80.0,
+                child: snapshot.hasData ? Text('Im visible $index') : Text('You cant see me $index'),
+              );
+            },
+          );
+        }
+    );
+  }
+  
+  getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'hi',
+    );
+  }
+}
