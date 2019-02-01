f110441f62d35cc3407dbf690acdc424f19415d6
Vitaly-V <gtopbox@gmail.com>
2019-02-01 12:52:05 +0200

    Add BLOC

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,36 +1,37 @@
 import 'package:flutter/material.dart';
+import '../blocs/stories_provider.dart';
+import '../widgets/news_list_tile.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+    bloc.fetchTopIds();
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
+      body: buildList(bloc),
     );
   }
-  
-  Widget buildList() {
-    return ListView.builder(
-        itemCount: 1000,
-        itemBuilder: (context, int index) {
-          return FutureBuilder(
-            future: getFuture(),
-            builder: (context, snapshot) {
-              return Container(
-                height: 80.0,
-                child: snapshot.hasData ? Text('Im visible $index') : Text('You cant see me $index'),
-              );
-            },
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (context, AsyncSnapshot<List<int>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child:  CircularProgressIndicator(),
           );
         }
+
+        return ListView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, int index) {
+            return NewsListTile(itemId: snapshot.data[index]);
+          },
+        );
+      }
     );
   }
-  
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
-    );
-  }
+
 }
