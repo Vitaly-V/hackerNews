a218aaf4a268796825b3f76780572388dcbf194f
Vitaly-V <gtopbox@gmail.com>
2019-02-21 16:49:37 +0200

    News detail

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -23,10 +23,12 @@ class CommentsBloc {
   _commentsTransformer() {
     return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
         (cache, int id, index) {
+          print(index);
           cache[id] = _repository.fetchItem(id);
           cache[id].then((ItemModel item) {
             item.kids.forEach((kidId) => fetchItemWithComments(kidId));
           });
+          return cache;
         },
       <int, Future<ItemModel>>{}
     );
